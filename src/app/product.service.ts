dd06eb2df882e0e02cd5c18aad3a02167942d78c
Vijay Vasanthapuram <vijay.vasanthapuram@arris.com>
2019-11-17 19:09:50 -0500

    Add TS types and interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/observable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -9,8 +11,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map(response =>  response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => (Album) response.json());
   }
 
 }
